1eb258ef6df311063a70fecebdc081049d5fca1e
Karim-47 <karim.pc.381@gmail.com>
2025-12-05 18:22:45 +0500

    Add files via upload

---
+++ L02 Homework.py
@@ -0,0 +1,21 @@
+print("Hi I am Raj and I am gonna ask you your birthdays.")
+
+#collecting birthdays
+Friend1 = input("What is your birthday?")
+print()
+Friend2 = input("What is your birthday?")
+print()
+Friend3 = input("What is your birthday?")
+print()
+Friend4 = input("What is your birthday?")
+print()
+Friend5 = input("What is your birthday?")
+print()
+
+#displaying the birthdays for each friend
+print(f"Friend1 your birthday is on, {Friend1}\n"+
+      f"Friend2 your birthday is on, {Friend2}\n"+
+      f"Friend3 your birthday is on, {Friend3}\n"+
+      f"Friend4 your birthday is on, {Friend4}\n"+
+      f"Friend5 your birthday is on, {Friend5}\n")
+
