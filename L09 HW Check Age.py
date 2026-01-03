ace3d3d178b8647a61038edf469597da5f81543a
Karim-47 <karim.pc.381@gmail.com>
2026-01-03 17:25:13 +0500

    Add files via upload

---
+++ L09 HW Check Age.py
@@ -0,0 +1,9 @@
+Age = int(input("Enter your age: "))
+
+if Age >= 10:
+    if Age<= 20:
+        print("Your age is perfect!")
+    else:
+        print("You are too old.")
+else:
+    print("You are too young.")
