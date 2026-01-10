3ee75107194787066c08c88db045ae8b80fedb2d
Karim-47 <karim.pc.381@gmail.com>
2026-01-10 20:55:48 +0500

    L12 Mid Product.py

--- L12 Mid Product.py
+++ L12 Mid Product.py
@@ -1,17 +1,18 @@
 num = input("Enter a number: ")
-if len(num) >=4:
+length=len(num)
+if length >=4:
     even = 0
-    if (len(num)%2)==0:
+    if (length%2)==0:
         even=1
 
     if even:
-        mid1 = int(num[len(num)/2])
-        mid2 = int(num[(len(num)/2)+1])
+        mid1 = int(num[(int(length/2))-1])
+        mid2 = int(num[int(length/2)])
         product = mid1  * mid2     
 
     if not even:
-        mid1 = int(num[(len(num)//2)+1])
-        mid2 = int(num[(len(num)//2)+2])
+        mid1 = int(num[int(length//2)])
+        mid2 = int(num[int(length//2)+1])
         product = mid1 * mid2
 
     print(f"The product of mid digits is ({mid1} * {mid2} = {product})")
