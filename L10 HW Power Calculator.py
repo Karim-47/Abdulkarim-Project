89650a17c4edf8d82a69944b9240e18bceda3d6a
Karim-47 <karim.pc.381@gmail.com>
2026-01-17 17:38:09 +0500

    Add files via upload

---
+++ L10 HW Power Calculator.py
@@ -0,0 +1,5 @@
+import math as m
+number = int(input("Enter the number ad the base: "))
+pow = int(input("Enter the power you need to apply to the base: "))
+
+print(f"The result is {number}^{pow} =", m.pow(number,pow))
