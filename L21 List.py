1c6a63dc498a4494d0c8d74ab7da662563ade966
Karim-47 <karim.pc.381@gmail.com>
2026-02-14 20:27:20 +0500

    Add files via upload

---
+++ L21 List.py
@@ -0,0 +1,23 @@
+print("empty list")
+empty_list = []
+print(empty_list)
+print()
+
+numbers = [1,2,3,4,5]
+print("List of numbers")
+print(numbers)
+print()
+
+triples = [5,1,7] * 3
+print("Triple List")
+print(triples)
+print()
+
+aList = [100,200,300,400]
+print("original list")
+print(aList)
+print()
+
+print("Reversed list")
+aList = aList[::-1]
+print(aList)
